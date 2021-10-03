62b8165e4cab966c98d234620d4b2893b405b04f
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 19:31:30 -0400

    exercise29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a quick calculator that prompts for the rate of return on an investment and
+calculates how many years it will take to double your investment.
+ */
+
+public class Solution29 {
+
+    /*
+    create scanner object
+    ask user to give the interest rate
+    read in the given input
+    check to make sure the input is allowed
+    if allowed to calculate the amount of years it will take to double
+    print out calculations
+     */
+}
