4bdf446966520e7e34dcf5d559ad903caec401ca
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 18:25:54 -0400

    exercise 27

---
+++ exercise 27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a program that prompts for a first name, last name, employee ID, and ZIP code.
+Ensure that the input is valid according to these rules:
+The first name must be filled in.
+The last name must be filled in.
+The first and last names must be at least two characters long.
+An employee ID is in the format AA-1234. So, two letters, a hyphen, and four numbers.
+The ZIP code must be a number.
+Display appropriate error messages on incorrect data.
+ */
+
+public class Solution27 {
+
+    /*
+    create scanner object
+    prompt the user to input information
+    read in all the info from the user
+    validate all the information given
+    print out if there were errors or not
+     */
+}
