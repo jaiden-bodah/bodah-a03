3c6ae957b21de0833bffd81e4f22879c2b56f079
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 15:59:10 -0400

    exercise 25

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that determines the complexity of a given password based on these rules:
+A very weak password contains only numbers and is fewer than eight characters.
+A weak password contains only letters and is fewer than eight characters.
+A strong password contains letters and at least one number and is at least eight characters.
+A very strong password contains letters, numbers, and special characters and is at least eight characters.
+ */
+
+public class Solution25 {
+
+    /*
+    create a scanner object
+    have the user input a potential password
+    check if they used upper or lower case letters
+    check if they used numbers
+    check if they used special symbols
+    use this information to calculate if the password is strong or not
+    return password strength
+     */
+}
