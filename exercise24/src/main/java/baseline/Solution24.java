a255da1d6c7b24a9203fc4f9bc4f24aa50baf227
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 15:00:00 -0400

    exerxise 24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that compares two strings and determines if the two strings are anagrams.
+The program should prompt for both input strings and display the output as shown in the example that follows.
+ */
+
+public class Solution24 {
+
+    /*
+    create a scanner object
+    create the function isAnagram
+    prompt the user to provide two strings
+    check if the strings are the same length
+    check if they are anagrams
+    return if the two words are anagrams or not
+     */
+}
