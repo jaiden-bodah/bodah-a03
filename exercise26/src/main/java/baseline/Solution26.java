123e59a22f3fcd89583d9aac963924ea12027cc1
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 17:16:57 -0400

    exercise 26

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a program that will help you determine how many months it will take to pay off a credit card balance.
+The program should ask the user to enter the balance of a credit card, the APR of the card, and their monthly payment.
+The program should then return the number of months needed (rounded up to the next integer value).
+ */
+
+import java.util.Scanner;
+
+public class Solution26 {
+
+    /*
+    create scanner object
+    have the user input the balance, APR, and payment
+    use the input to calculate the amount of months until card is paid off
+    print months till payment is done
+     */
+}
+
