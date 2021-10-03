adddfd6ff7a37d512872623ad7331ea507104bb7
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 18:53:33 -0400

    exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a program that prompts the user for five numbers and computes the total of the numbers.
+ */
+
+public class Solution28 {
+
+    /*
+    create scanner object
+    prompt the user to input 5 numbers
+    create a loop for gathering the numbers
+    take the 5 numbers the user inputted and calculate the total
+    use that total to find the average of the numbers
+    output average
+     */
+}
