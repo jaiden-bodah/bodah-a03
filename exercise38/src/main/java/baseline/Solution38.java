3d6daaaa626a38e61e0adb62b6afa987de06bee1
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 23:56:30 -0400

    exercise38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that generates a secure password.
+Prompt the user for the minimum length, the number of special characters, and the number of numbers.
+Then generate a password for the user using those inputs.
+ */
+
+public class Solution38 {
+
+    /*
+    create a scanner object
+    have user input password length
+    enter amount of special characters
+    enter amount of numbers
+    create strings with different letters, numbers, characters
+    have a function that returns a random password
+    print password
+     */
+}
