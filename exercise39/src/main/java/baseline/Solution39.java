031e5ba11612f4fb9a72382fa2bf58391cfa2766
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-04 03:04:45 -0400

    exercise29

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Given the following data set,
+create a program that sorts all employees by last name and prints them to the screen in a tabular format.
+ */
+
+public class Solution39 {
+
+    /*
+    input the data of the employees
+    create functions that set each individual piece of the table
+    setup an array and sort the employees
+    construct a table
+     */
+}
