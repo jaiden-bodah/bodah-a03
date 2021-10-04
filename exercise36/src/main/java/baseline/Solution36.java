0677d38727a66da9024c7a92bfc759d35cd84ea7
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 23:28:00 -0400

    exercise36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a program that prompts for response times from a website in milliseconds.
+It should keep asking for values until the user enters “done.”
+ */
+
+public class Solution36 {
+
+    /*
+    create scanner object
+    create an arraylist
+    create a while loop
+    prompt user for input
+    keep asking for inputs until user types done
+    print out the numbers, average, min, max, and standard deviation
+     */
+}
