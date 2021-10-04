aa008ddedce04e1eb808f773540167987d0044bc
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 20:35:59 -0400

    exercise31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+ Create a program that prompts for your age and your resting heart rate.
+ Use the Karvonen formula to determine the target heart rate based on a range of intensities from 55% to 95%.
+ Generate a table with the results as shown in the example output.
+ */
+
+public class Solution31 {
+
+    /*
+    create scanner object
+    prompt user to input resting heart rate and age
+    print out resting heart rate and age
+    use inputs to calculate heart rate at different intensities
+    keep calculating till 95%
+    output calculations
+     */
+}
