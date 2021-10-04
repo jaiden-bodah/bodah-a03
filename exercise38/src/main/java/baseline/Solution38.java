6bf2c44c806f2e78d2a966883976a410c04b76ac
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-04 00:03:23 -0400

    exercise 38 2

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -11,6 +11,11 @@ Prompt the user for the minimum length, the number of special characters, and th
 Then generate a password for the user using those inputs.
  */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution38 {
 
     /*
@@ -22,4 +27,79 @@ public class Solution38 {
     have a function that returns a random password
     print password
      */
+
+    public static void main(String[] args)
+    {
+        // create an object of Scanner class for user input
+        Scanner in = new Scanner(System.in);
+
+        // prompt user to enter password length
+        System.out.print("What's the minimum length? ");
+        int size = Integer.parseInt(in.nextLine());
+
+        // enter number of special characters in password
+        System.out.print("How many special characters? ");
+        int special = Integer.parseInt(in.nextLine());
+
+        // enter number of numbers in password
+        System.out.print("How many numbers? ");
+        int numbers = Integer.parseInt(in.nextLine());
+
+        // make a function call that will return a random password
+        // display that password
+        System.out.println("Your password is " + generatePassword(size, special, numbers));
+    }
+
+    // this method takes length, required special Characters and numbers as input parameter
+    // and the generate and return a random password
+    public static String generatePassword(int length, int special, int nums)
+    {
+        // declare String of letters
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        // declare a String of Characters
+        String specialCharacters = "!@#$%^&*";
+
+        // declare a String of numbers
+        String numbers = "1234567890";
+
+        // create an object of Random class
+        Random random = new Random();
+
+        // declare an ArrayList of Characters
+        ArrayList<Character> chars = new ArrayList<>();
+
+        // now, run a loop for required number of special characters times
+        for(int i=0; i<special; i++){
+            // find random special Character and add it to the list
+            chars.add(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+        }
+
+        // now, run a loop for required number of numerals times
+        for(int i=0; i<nums; i++){
+            // find random numeral and add it to the list
+            chars.add(numbers.charAt(random.nextInt(numbers.length())));
+        }
+
+        // now, run a loop for the remaining length of password
+        for(int i=chars.size()-1; i<length; i++){
+            // find random letter and add it to the list
+            chars.add(letters.charAt(random.nextInt(letters.length())));
+        }
+
+        // now, shuffle the elements of the list
+        Collections.shuffle(chars);
+
+        // now, declare a empty string
+        String password = "";
+
+        // loop to concatenate all list elements to the result String
+        for(Character s : chars)
+        {
+            password += Character.toString(s);
+        }
+
+        // return result String
+        return password;
+    }
 }
---
+++ exercise38/src/test/java/baseline/Solution38Test.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution38Test {
+
+    @Test
+    Object main() {
+
+        public static String[] generateRandomWords(int numberOfWords){
+            String[] randomStrings = new String[numberOfWords];
+            Random random = Random();
+            return null;
+        }
+    }
+}
