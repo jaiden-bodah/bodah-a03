ad6cf9d39150103a7fd7699b1aff759df0ce23d0
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 22:54:52 -0400

    exercise 35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that picks a winner for a contest or prize drawing.
+Prompt for names of contestants until the user leaves the entry blank.
+Then randomly select a winner.
+ */
+
+public class Solution35 {
+
+    /*
+    create scanner object
+    get user input of names
+    create an arraylist to store the names
+    create a loop to take the names
+    once you get the name add the name to the list
+    pick a random number and sue that to determine the winner
+    print out the winner
+     */
+}
