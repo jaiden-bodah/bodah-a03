6cab35d855bc34783b74682a63fedfad4372d910
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 21:18:30 -0400

    exercise32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Write a Guess the Number game that has three levels of difficulty.
+The first level of difficulty would be a number between 1 and 10.
+The second difficulty set would be between 1 and 100.
+The third difficulty set would be between 1 and 1000.
+Prompt for the difficulty level, and then start the game.
+The computer picks a random number in that range and prompts the player to guess that number.
+Each time the player guesses, the computer should give the player a hint as to whether the number is too high or too low.
+The computer should also keep track of the number of guesses.
+Once the player guesses the correct number, the computer should present the number of guesses and ask if the player would like to play again.
+ */
+
+public class Solution32 {
+
+    /*
+    create scanner object
+    prompt the user for what difficulty they would like to play
+    choose a number depending on the difficulty
+    have the user input guesses until they guess correctly
+    if they are wrong give them a hint
+    if they are right tell them they got it and the amount of tries
+    asks them if they would like to play again
+     */
+}
