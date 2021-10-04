7f7d8e4aa3a64a3664fb59f0651306f9ad031d9f
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 23:43:14 -0400

    exercise27

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that prompts for a list of numbers, separated by spaces.
+Have the program print out a new list containing only the even numbers.
+ */
+
+public class Solution37 {
+
+    /*
+    create a scanner object
+    have user provide list of numbers separated by spaces
+    create a string array to store data
+    array will only return even numbers
+     */
+}
