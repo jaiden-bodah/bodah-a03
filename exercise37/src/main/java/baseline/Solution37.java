c7611b1080740721c2d78b8b723b976c93489b24
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 23:49:10 -0400

    exercise37 2

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -10,6 +10,9 @@ Create a program that prompts for a list of numbers, separated by spaces.
 Have the program print out a new list containing only the even numbers.
  */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution37 {
 
     /*
@@ -18,4 +21,47 @@ public class Solution37 {
     create a string array to store data
     array will only return even numbers
      */
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a list of numbers, separated by spaces: ");
+        String data = in.nextLine();
+
+        String list[] = data.split(" ");
+
+        Integer evenList[] = filterEvenNumbers(list);
+
+        System.out.print("The even numbers are ");
+        for(int i=0; i<evenList.length; i++){
+            if(i < evenList.length-1){
+                System.out.print(evenList[i] + " ");
+            }
+            else
+            {
+                System.out.print(evenList[i] + ".");
+            }
+        }
+    }
+
+    public static Integer[] filterEvenNumbers(String list[]) {
+
+        ArrayList<Integer> newList = new ArrayList<>();
+
+        for(int i=0; i<list.length; i++){
+
+            int num = Integer.parseInt(list[i]);
+
+            if(num % 2 == 0){
+
+                newList.add(num);
+            }
+        }
+
+        Integer[] arr = new Integer[newList.size()];
+        arr = newList.toArray(arr);
+
+        return arr;
+    }
 }
---
+++ exercise37/src/test/java/baseline/Solution37Test.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution37Test {
+
+    @Test
+    void main() {
+
+        public boolean isEvenNumber(int number){
+
+            boolean result = false;
+            if(number%2 == 0){
+                result = true;
+            }
+            return result;
+        }
+    }
+}
