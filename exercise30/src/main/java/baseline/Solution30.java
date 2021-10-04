29c40d31f388d421096fe7cf8a9b1589df1f437f
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 20:23:02 -0400

    exercise 30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+ */
+
+public class Solution30 {
+
+    /*
+    create a function that produces the multiplication table
+    use a nested loop
+    align each column of the table
+     */
+}
---
+++ exercise30/src/test/java/baseline/Solution30Test.java
@@ -0,0 +1,19 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution30Test {
+
+    @Test
+    void printMultiplicationTable() {
+        System.out.printf("%4d |",1);
+
+        System.out.printf("%4d |",12);
+
+        System.out.printf("%4d |",123);
+
+        System.out.printf("%4d |",1234);
+    }
+}
