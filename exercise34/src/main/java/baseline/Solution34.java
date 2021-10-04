62c3ad65597cd2e4f2e6c79a563321d62c174239
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 22:32:18 -0400

    exercise34

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a small program that contains a list of employee names.
+Print out the list of names when the program runs the first time.
+Prompt for an employee name and remove that specific name from the list of names.
+Display the remaining employees, each on its own line.
+ */
+
+public class Solution34 {
+
+    /*
+    create an arraylist of employees
+    prints the employee list
+    create scanner object
+    get the employee's name that is to be removed
+    print out employee list again, now with the selected name removed
+     */
+}
