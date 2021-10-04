61c790de8c43ef78aef10c6ad9f19b599775ece2
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-03 22:05:30 -0400

    exercise 33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Create a Magic 8 Ball game that prompts for a question and then displays either
+"Yes," "No," "Maybe," or "Ask again later."
+ */
+
+public class Solution33 {
+
+    /*
+    create random object
+    create scanner object
+    prompt user to ask questions
+    choose random answer
+    print response
+     */
+
+
+}
