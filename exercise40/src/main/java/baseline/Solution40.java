b9f3ede1e678f8cb1f285e624ec94a7d86ae4e0a
jaiden-bodah <jaidenbodah1@gmail.com>
2021-10-04 03:10:31 -0400

    exercise 40

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Jaiden Bodah
+ */
+
+package baseline;
+
+/*
+Given the following data set create a program that lets a user locate all records that match the search string by
+comparing the search string to the first or last name field.
+ */
+
+public class Solution40 {
+
+    /*
+    create a list of employees
+    create scanner object
+    allow for user input to search the database
+    provide user information based upon their query
+     */
+}
